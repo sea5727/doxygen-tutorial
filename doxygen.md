a089928cc2bdb63c3d0d5cb973e67dfe9ed2dfed
ysh8361 <sea5727@naver.com>
2021-01-23 15:16:53 +0900

    commit

---
+++ doxygen.md
@@ -0,0 +1,4 @@
+doxygen -s -g doxyfile 독시젠 default config 생성
+
+doxygen doxyfile이름
+새로 적용
---
+++ main.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-01-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <iostream>
+
+/**
+ * @brief add
+ * 
+ * @param a a
+ * @param b b
+ * @return int  a+b
+ */
+int add(int a, int b){
+    return a + b;
+}
+
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n";
+}
